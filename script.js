b43632573bbc3d4048ba673daa4b237722c2f90a
leoncol <camiloleon196@gmail.com>
2025-07-30 14:32:22 -0500

    Add dialog form and DOM logic

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const myLibrary = []; // Empty array to save the books to.
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + #open-button");
+const closeButton = document.querySelector("dialog #close-button");
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("book-pages");
+const bookStatus = document.querySelector("#book-status");
+
+function retrieveBookStatus(){
+    if (bookStatus.value == "true"){
+        bookStatus = true;
+    } else {
+        bookStatus = false;
+    }
+}
+
+
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+
 function Book(title, author, pages, read){ // This is the constructor for the books.
     this.id = crypto.randomUUID();
     this.title = title;
@@ -15,8 +44,8 @@ function Book(title, author, pages, read){ // This is the constructor for the bo
     }
 }
 
-function addBookToLibrary(title, author, pages, read) { // This calls the constructor and adds the books to the library.
-  let newBook = new Book(title, author, pages, read);
+function addBookToLibrary() { // This calls the constructor and adds the books to the library.
+  let newBook = new Book(bookTitle, bookAuthor, bookPages, bookStatus);
   myLibrary.push(newBook);
 }
 
